7105a58fa51dc4e99e8424520d5328e3c75cd86f
Tahir Raza <tahir.raza@accesspay.com>
2018-05-31 13:05:01 +0100

    initialized the test program

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace chapter2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(var r in Assembly.GetEntryAssembly().GetReferencedAssemblies())
+            {
+                var a = Assembly.Load(new AssemblyName(r.FullName));
+                int methodCount = 0;
+                foreach(var t in a.DefinedTypes)
+                {
+                    methodCount += t.GetMethods().Count();
+                }
+                Console.WriteLine($"{a.DefinedTypes.Count() :N0} types " + $"with {methodCount :N0} methods in {r.Name} assembly.");
+            }
+        }
+    }
+}
